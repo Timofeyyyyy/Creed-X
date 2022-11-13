747d785c13e80da676150f7dacdaa97c0b61779a
Timofeyyyyy <116093275+Timofeyyyyy@users.noreply.github.com>
2022-11-13 21:25:35 +0200

    end home page (почти окончил)

---
+++ my.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelector(".nav-link").forEach((n) => n.add);
